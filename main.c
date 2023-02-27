23d93d6d69ec251098712d5df8e065ea09416da2
Ricardo Vinicius <ricardoviniciusaf@gmail.com>
2023-02-27 00:56:20 -0300

    adding changes

--- main.c
+++ main.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <strings.h>
+
 #define MAX_LENGTH_PHRASE 4096;
 #define MAX_LENGTH_CLAUSE 256;
 #define flag uint8_t;
@@ -22,13 +26,58 @@ atomic;
 
 int main(void)
 {
+    Se fizer sol então vou a escola
+    0   1      2   3     4 5  6
 
+    if(strcmp(v[0], "Se") = 0)
+    {
+        inicio = i;
+        do
+        {
+            i++;
+        }
+        while(strcmp(v[i], "então") = 0)
+        fim  = i;
+
+        sentence *n = malloc(sizeof(sentence))
+
+
+
+    }
+}
+
+
+char* split_string(char *v)
+{
+    char *names[] = {
+      "Zara Ali",
+      "Hina Ali",
+      "Nuha Ali",
+      "Sara Ali"
+   };
+    while(*v != '\0')
+    {
+
+    }
 }
 
 // function that reads a sentence in natural language and returns a sentence struct 
-sentence read_phrase()
+sentence read_phrase(char *v,  )
 {
-    //if finds a "if"
-    //creates a sentence
+    // TODO: escrever uma função que consiga ler uma string e criar uma sentença lógica a partir dela
 
+    // TODO: escrever um procedimento que leia negações
+    while(v[i] != " ")
+    {
+
+    }
+    // TODO: escrever um procedimento que leia implicação
+
+    // TODO: escrever um procedimento que leia bi-implicação 
+
+    // TODO: escrever um procedimento que leia conjunção
+
+    // TODO: escrever um procedimento que leia disjunção
+
+    // TODO: escrever um procedimento que leia sentenças atômicas
 }
