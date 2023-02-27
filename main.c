83f3dada329a36ec30d149487920ff44f7d0416d
Ricardo Vinicius <ricardoviniciusaf@gmail.com>
2023-02-27 12:45:53 -0300

    adding main

--- main.c
+++ main.c
@@ -5,17 +5,7 @@
 
 #define MAX_LENGTH_PHRASE 4096;
 #define MAX_LENGTH_CLAUSE 256;
-#define flag uint8_t;
-
-typedef struct sentence
-{
-    sentence first; // if is a atomic sentence, represents the sentence itself
-    sentence next; // NULL if is a atomic sentence
-    flag operator; // only if is a complex sentence
-    atomic atomic; // only if is a atomic sentence
-    flag negative; // flag that means if the sentence is negatived or not; 0 = non negatived, 1 = negatived
-}
-sentence;
+#define flag "int";
 
 typedef struct atomic
 {
@@ -24,10 +14,55 @@ typedef struct atomic
 }
 atomic;
 
+typedef struct sentence
+{
+    struct sentence first; // if is a atomic sentence, represents the sentence itself
+    struct sentence next; // NULL if is a atomic sentence
+    flag operator; // only if is a complex sentence
+    atomic atomic; // only if is a atomic sentence
+    flag negative; // flag that means if the sentence is negatived or not; 0 = non negatived, 1 = negatived
+}
+sentence;
+
+
+
+char* split_string(char *v);
+
 int main(void)
 {
-    Se fizer sol então vou a escola
-    0   1      2   3     4 5  6
+    char *n = "Se fizer sol comprarei chocolate";
+
+    split_string(n);
+    
+}
+
+
+char* split_string(char *v)
+{
+    // TODO: Função pra splitar a string em um array de palavras na memória 
+ 
+    // Returns first token
+    char *token = strtok(v, " ");
+   
+    while (token != NULL)
+    {
+        token = strtok(NULL, " ");
+        printf("%s\n", token);
+    }
+ 
+}
+
+// function that reads a sentence in natural language and returns a sentence struct 
+sentence read_phrase(char *v  )
+{
+    // TODO: escrever uma função que consiga ler uma string e criar uma sentença lógica a partir dela
+
+    // TODO: escrever um procedimento que leia negações
+    int i;
+    while(v[i] != " ")
+    {
+
+    }
 
     if(strcmp(v[0], "Se") = 0)
     {
@@ -42,33 +77,6 @@ int main(void)
         sentence *n = malloc(sizeof(sentence))
 
 
-
-    }
-}
-
-
-char* split_string(char *v)
-{
-    char *names[] = {
-      "Zara Ali",
-      "Hina Ali",
-      "Nuha Ali",
-      "Sara Ali"
-   };
-    while(*v != '\0')
-    {
-
-    }
-}
-
-// function that reads a sentence in natural language and returns a sentence struct 
-sentence read_phrase(char *v,  )
-{
-    // TODO: escrever uma função que consiga ler uma string e criar uma sentença lógica a partir dela
-
-    // TODO: escrever um procedimento que leia negações
-    while(v[i] != " ")
-    {
 
     }
     // TODO: escrever um procedimento que leia implicação
